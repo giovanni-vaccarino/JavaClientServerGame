edf6306cb8e42fe3cf6b5c6a086ff6709e75ceae
Gianni <giovannivaccarino03@gmail.com>
2024-03-19 16:41:08 +0100

    deletion of useless classes

--- src/main/java/polimi/ingsoft/DrawableDeck.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.polimi.ingsoft;
-
-public interface DrawableDeck<T extends Drawable> extends Queue, Deck<T> {
-}
--- src/main/java/polimi/ingsoft/GameDeck.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.polimi.ingsoft;
-
-public class GameDeck implements DrawableDeck<DrawableGameCard> {
-}
