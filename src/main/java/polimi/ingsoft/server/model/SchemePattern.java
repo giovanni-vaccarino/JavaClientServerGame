283cd6fdfcdd7c4757914275fb52e0af8f23013d
Simo <simone.zacchi@mail.polimi.it>
2024-04-17 18:41:24 +0200

    testing on schemepattern various improvement on schemepattern class to do: fix issues with schemepattern return values while schemepattern's function works properly, the return values are somehow divergent

--- src/main/java/polimi/ingsoft/server/model/SchemePattern.java
+++ src/main/java/polimi/ingsoft/server/model/SchemePattern.java
@@ -13,21 +13,21 @@ public class SchemePattern implements Pattern {
         if(setvisited)visited.put(actualCoordinates,true);
         if (setvisited) {
             if (board.getCard(actualCoordinates).getUpLeftCorner()!=null
-                    &&visited.get(actualCoordinates.sum(new Coordinates(-1, 1)))==null)
+                    &&!visited.containsKey(actualCoordinates.sum(new Coordinates(-1, 1))))
                 count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, 1))));
             if (board.getCard(actualCoordinates).getUpRightCorner() != null
-                    &&visited.get(actualCoordinates.sum(new Coordinates(1, 1)))==null)
+                    &&!visited.containsKey(actualCoordinates.sum(new Coordinates(1, 1))))
                 count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, 1))));
             if (board.getCard(actualCoordinates).getBottomLeftCorner() != null
-                    &&visited.get(actualCoordinates.sum(new Coordinates(-1, -1)))==null)
+                    &&!visited.containsKey(actualCoordinates.sum(new Coordinates(-1, -1))))
                 count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, -1))));
             if (board.getCard(actualCoordinates).getBottomRightCorner() != null
-                    &&visited.get(actualCoordinates.sum(new Coordinates(1, -1)))!=null)
+                    &&!visited.containsKey(actualCoordinates.sum(new Coordinates(1, -1))))
                 count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, -1))));
             }
         if(!actualCoordinates.equals(new Coordinates(0,0))
                 &&board.getCard(actualCoordinates).getColor().equals(order.get(actualLink).getColor())
-                    &&used.get(actualCoordinates)==null){
+                    &&!used.containsKey(actualCoordinates)){
             if(actualLink<maxSize) { //rivedere riguardo le diagonali
 //                if (order.get(1).getPosFromBegin().equals(new Coordinates(1, 1))) {
 //                    if (board.getCard(actualCoordinates.sum(new Coordinates(1, 1)))!=null&&board.getCard(actualCoordinates.sum(new Coordinates(1, 1))).getColor() == order.get(actualLink).getColor()
@@ -53,13 +53,13 @@ public class SchemePattern implements Pattern {
 //                        count=count+getMatch(used,false,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, -1)));
 //                    else if(visited.get(actualCoordinates.sub(new Coordinates(1,-1))))count = count + getMatch(used,false,visited,board, count, actualLink + 1, actualCoordinates.sub(new Coordinates(1, -1)));
 //                }
-              /*else*/ count = count + getMatch(used,false, visited, board, count, actualLink + 1, actualCoordinates.sum(order.get(actualLink + 1).getPosFromBegin()));
-            } else if(actualLink==maxSize&&board.getCard(actualCoordinates).getColor().equals(order.get(actualLink).getColor())&&used.get(board.getCard(actualCoordinates))==null){
-                    if(order.get(1).getPosFromBegin().equals(new Coordinates(1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, 1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(1, 1))){
-                        for(int i=0;i<maxSize+1;i++)used.put(actualCoordinates.sum(order.get(i).getPosFromBegin()),true);
-                    }else {
+              /*else*//* count = count +*/ return getMatch(used,false, visited, board, count, actualLink + 1, actualCoordinates.sub(order.get(actualLink).getPosFromBegin()).sum(order.get(actualLink + 1).getPosFromBegin()));
+            } else if(board.getCard(actualCoordinates).getColor().equals(order.get(actualLink).getColor())&&!used.containsKey(board.getCard(actualCoordinates))){
+                    //if(order.get(1).getPosFromBegin().equals(new Coordinates(1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, 1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(1, 1))){
+                        //for(int i=0;i<maxSize+1;i++)used.put(actualCoordinates.sum(order.get(i).getPosFromBegin()),true);
+                    //}else {
                         for(int i=0;i<maxSize+1;i++)used.put(actualCoordinates.sub(order.get(actualLink).getPosFromBegin()).sum(order.get(i).getPosFromBegin()),true);
-                     }
+                     //}
                     System.out.println(actualCoordinates.getX()+","+actualCoordinates.getY());
                     return 1;
                 }
