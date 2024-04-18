8425df1450ff5c1d950f2b5166ec7274a11cbbf2
Simo <simone.zacchi@mail.polimi.it>
2024-04-18 08:50:10 +0200

    update on schemepattern class now working properly

--- src/main/java/polimi/ingsoft/server/model/SchemePattern.java
+++ src/main/java/polimi/ingsoft/server/model/SchemePattern.java
@@ -9,21 +9,21 @@ public class SchemePattern implements Pattern {
         this.order=order;
     }
     private int getMatch(HashMap<Coordinates,Boolean> used,boolean setvisited,HashMap<Coordinates,Boolean> visited, Board board, int count, int actualLink, Coordinates actualCoordinates) {
-        if (board.getCard(actualCoordinates/*.sum(order.get(actualLink).getPosFromBegin())*/) == null) return 0;
+        if (board.getCard(actualCoordinates/*.sum(order.get(actualLink).getPosFromBegin())*/) == null) return count;//0;
         if(setvisited)visited.put(actualCoordinates,true);
         if (setvisited) {
             if (board.getCard(actualCoordinates).getUpLeftCorner()!=null
                     &&!visited.containsKey(actualCoordinates.sum(new Coordinates(-1, 1))))
-                count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, 1))));
+                count = /*count +*/ (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, 1))));
             if (board.getCard(actualCoordinates).getUpRightCorner() != null
                     &&!visited.containsKey(actualCoordinates.sum(new Coordinates(1, 1))))
-                count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, 1))));
+                count = /*count +*/ (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, 1))));
             if (board.getCard(actualCoordinates).getBottomLeftCorner() != null
                     &&!visited.containsKey(actualCoordinates.sum(new Coordinates(-1, -1))))
-                count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, -1))));
+                count = /*count +*/ (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(-1, -1))));
             if (board.getCard(actualCoordinates).getBottomRightCorner() != null
                     &&!visited.containsKey(actualCoordinates.sum(new Coordinates(1, -1))))
-                count = count + (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, -1))));
+                count = /*count +*/ (getMatch(used,true,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, -1))));
             }
         if(!actualCoordinates.equals(new Coordinates(0,0))
                 &&board.getCard(actualCoordinates).getColor().equals(order.get(actualLink).getColor())
@@ -53,17 +53,17 @@ public class SchemePattern implements Pattern {
 //                        count=count+getMatch(used,false,visited,board, count, actualLink, actualCoordinates.sum(new Coordinates(1, -1)));
 //                    else if(visited.get(actualCoordinates.sub(new Coordinates(1,-1))))count = count + getMatch(used,false,visited,board, count, actualLink + 1, actualCoordinates.sub(new Coordinates(1, -1)));
 //                }
-              /*else*//* count = count +*/ return getMatch(used,false, visited, board, count, actualLink + 1, actualCoordinates.sub(order.get(actualLink).getPosFromBegin()).sum(order.get(actualLink + 1).getPosFromBegin()));
+              /*else*//* count = count +*/ return /*count+*/getMatch(used,false, visited, board, count, actualLink + 1, actualCoordinates.sub(order.get(actualLink).getPosFromBegin()).sum(order.get(actualLink + 1).getPosFromBegin()));
             } else if(board.getCard(actualCoordinates).getColor().equals(order.get(actualLink).getColor())&&!used.containsKey(board.getCard(actualCoordinates))){
                     //if(order.get(1).getPosFromBegin().equals(new Coordinates(1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, 1))||order.get(1).getPosFromBegin().equals(new Coordinates(-1, -1))||order.get(1).getPosFromBegin().equals(new Coordinates(1, 1))){
                         //for(int i=0;i<maxSize+1;i++)used.put(actualCoordinates.sum(order.get(i).getPosFromBegin()),true);
                     //}else {
                         for(int i=0;i<maxSize+1;i++)used.put(actualCoordinates.sub(order.get(actualLink).getPosFromBegin()).sum(order.get(i).getPosFromBegin()),true);
                      //}
-                    System.out.println(actualCoordinates.getX()+","+actualCoordinates.getY());
-                    return 1;
-                }else return 0;
-            }else if(!setvisited)return 0;
+                /**///System.out.println(actualCoordinates.getX()+","+actualCoordinates.getY());
+                    return count+1;
+                }//else return 0;
+            }//else if(!setvisited)return 0;
         //return setvisited? count : 0;
         return count;
         }
