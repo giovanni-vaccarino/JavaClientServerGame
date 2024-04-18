e3515e0fe37d8f845c8773553ef51128774bd686
Simo <simone.zacchi@mail.polimi.it>
2024-04-18 10:53:48 +0200

    more fixes on questcard

--- src/main/java/polimi/ingsoft/server/model/QuestCard.java
+++ src/main/java/polimi/ingsoft/server/model/QuestCard.java
@@ -3,17 +3,16 @@ package polimi.ingsoft.server.model;
 import java.util.ArrayList;
 
 public class QuestCard extends Card implements Drawable, ConditionalPointsCard {
-    private SchemePattern pattern;
+
     private ArrayList<Link> links;
 
 
     public QuestCard(ArrayList<Link> links,int points){
         super(points);
         this.links=links;
-        this.pattern=new SchemePattern(links);
     }
 
-    public int getPoints(Board board){
-        return super.getScore()*this.pattern.getMatch(board);
+    public ArrayList<Link> getLink(){
+        return this.links;
         }
 }
