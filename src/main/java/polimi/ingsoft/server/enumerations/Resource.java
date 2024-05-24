d07b99844658500a332fe0cfe3a2de4fff870f16
Simo <simone.zacchi@mail.polimi.it>
2024-05-24 19:17:38 +0200

    new classes (some may be temp) now importing correctly resourcecards from json

--- src/main/java/polimi/ingsoft/server/enumerations/Resource.java
+++ src/main/java/polimi/ingsoft/server/enumerations/Resource.java
@@ -1,8 +1,13 @@
 package polimi.ingsoft.server.enumerations;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import polimi.ingsoft.server.model.Item;
-
+@JsonTypeName("Resource")
 public enum Resource implements Item {
-    LEAF, WOLF, MUSHROOM, BUTTERFLY;
+    LEAF("LEAF"), WOLF("WOLF"), MUSHROOM("MUSHROOM"), BUTTERFLY("BUTTERFLY");
+    private String value;
 
+     Resource(String value){
+        this.value=value;
+    }
 }
--- src/main/java/polimi/ingsoft/server/model/Pattern.java
+++ src/main/java/polimi/ingsoft/server/model/Pattern.java
@@ -1,7 +1,18 @@
 package polimi.ingsoft.server.model;
 
-import java.lang.reflect.GenericArrayType;
-import java.util.ArrayList;
+
+//@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.WRAPPER_OBJECT /*WRAPPER_OBJECT*/, property="cost", visible = true)
+//@JsonSubTypes({
+//        @JsonSubTypes.Type(value= HashMap.class, name="cost"),
+//        @JsonSubTypes.Type(value= Resource.class, name="MUSHROOM"),
+//        @JsonSubTypes.Type(value= Resource.class, name="BUTTERFLY"),
+//        @JsonSubTypes.Type(value= Resource.class, name="WOLF"),
+//        @JsonSubTypes.Type(value= Resource.class, name="LEAF"),
+//        @JsonSubTypes.Type(value= Object.class, name="FEATHER"),
+//        @JsonSubTypes.Type(value= Object.class, name="POTION"),
+//        @JsonSubTypes.Type(value= Object.class, name="SCROLL")
+//})
+
 
 public interface Pattern {
     int getMatch(Board board,Coordinates coordinates);
