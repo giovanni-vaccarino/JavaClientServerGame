37a55c7c3c266230c87f8a41d946e5f26678da5e
Andrea Viganò <viganoandrea02@gmail.com>
2024-06-26 15:18:08 +0200

    - Javadocs fixes for Command

--- src/main/java/polimi/ingsoft/server/common/command/Command.java
+++ src/main/java/polimi/ingsoft/server/common/command/Command.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.Serializable;
 
 /**
- *
+ * A command that can be executed with an argument
  *
  * @param <T> The parameter type of the execute function
  */
