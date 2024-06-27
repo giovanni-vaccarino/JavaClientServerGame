a3688c45bc0f20b364de9999c2285198c0747ae2
Gianni <giovannivaccarino03@gmail.com>
2024-06-28 00:41:53 +0200

    refactoring

--- src/main/java/polimi/ingsoft/server/common/MatchServer.java
+++ src/main/java/polimi/ingsoft/server/common/MatchServer.java
@@ -420,13 +420,13 @@ public class MatchServer implements VirtualMatchServer {
         Integer numOnlinePlayers = matchController.getNumOnlinePlayers();
         switch (numOnlinePlayers){
             case 0 -> {
-                logger.println("TIMER HAS BEEN SET. THE GAME IS GOING TO BE DELETED IN x SECONDS");
+                logger.println("TIMER HAS BEEN SET. THE GAME IS GOING TO BE DELETED IN 10 SECONDS");
                 matchController.getGameState().setBlockedMatchState(BLOCKED_MATCH_STATE.BLOCKED_NO_PLAYERS);
                 matchBlockedTimer.startTimer(10);
             }
 
             case 1 -> {
-                logger.println("TIMER HAS BEEN SET. THE GAME IS GOING TO BE DELETED IN x SECONDS");
+                logger.println("TIMER HAS BEEN SET. THE GAME IS GOING TO BE DELETED IN 10 SECONDS");
                 matchController.getGameState().setBlockedMatchState(BLOCKED_MATCH_STATE.BLOCKED_ONE_PLAYER);
                 matchBlockedTimer.startTimer(10);
             }
